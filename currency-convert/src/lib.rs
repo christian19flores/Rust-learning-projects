37af2b2385bc129715165e9ee6957bc86ab36ffe
Christian Flores <christian19flores@gmail.com>
2023-03-10 19:39:31 -0600

    working version of currency conversion

--- currency-convert/src/lib.rs
+++ currency-convert/src/lib.rs
@@ -9,48 +9,77 @@ pub struct Config {
 
 impl Config {
     pub fn build() -> Result<Config, &'static str> {
-
         let mut input_amount = String::new();
 
-        let mut cur_start = String::new();
+        let mut input_cur_start = String::new();
 
-        let mut cur_end = String::new();
-
+        let mut input_cur_end = String::new();
 
         // get amount
         println!("Enter currency amount:");
 
         io::stdin()
-            .read_line(&mut input_amount.parse::<f32>().unwrap())
+            .read_line(&mut input_amount)
             .expect("Failed to read input");
+
+        let amount = input_amount
+            .trim()
+            .parse::<f32>()
+            .expect("That's not a number");
 
         // get start currency
         println!("Enter start currency:");
 
         io::stdin()
-            .read_line(&mut cur_start);
+            .read_line(&mut input_cur_start)
             .expect("Failed to read input");
+
+        let cur_start = input_cur_start.as_str().trim().to_string();
 
         // get end currency
         println!("Enter end currency:");
 
         io::stdin()
-            .read_line(&mut cur_end)
-            .expect("Failed to read input");        
+            .read_line(&mut input_cur_end)
+            .expect("Failed to read input");
 
-        Ok(Config {  })
+        let cur_end = input_cur_end.as_str().trim().to_string();
+
+        Ok(Config {
+            amount,
+            cur_start,
+            cur_end,
+        })
     }
-}
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    pub fn run(&self) -> Result<(), Box<dyn Error>> {
+        let result = self.convert();
     
-    let result = convert(config.amount, config.cur_start, config.cur_end);
-
-    println!("Convert {0.0} {config.start} to {config.cur_end}: \n {0.0}", config.amount, result);
+        println!(
+            "Convert {1} {3} to {2}: \n {0}",
+            result, self.amount, self.cur_end, self.cur_start
+        );
     
-    Ok(())
-}
-
-pub fn convert(amount: f32, cur_start: String, cur_end: String) -> f32 {
+        Ok(())
+    }
     
+    pub fn convert(&self) -> f32 {
+        match self.cur_start.as_str() {
+            "usd" => {
+                if self.cur_end == "gbp" {
+                    self.amount * 0.85
+                } else {
+                    panic!("F's in the chat");
+                }
+            }
+            "gbp" => {
+                if self.cur_end == "usd" {
+                    self.amount * 1.15
+                } else {
+                    panic!("F's in the chat");
+                }
+            }
+            _ => todo!(),
+        }
+    }
 }
--- currency-convert/src/main.rs
+++ currency-convert/src/main.rs
@@ -1,15 +1,15 @@
 use std::env;
 use std::process;
 
-use factorial_finder::Config;
+use currency_convert::Config;
 
 fn main() {
-    let config = Config::build(env::args()).unwrap_or_else(|err| {
+    let config = Config::build().unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(e) = factorial_finder::run(config) {
+    if let Err(e) = config.run() {
         println!("Application error: {e}");
         process::exit(1);
     }
